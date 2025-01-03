da24da156b88dbacfb6077d3e93ba705ee3b5ae5
sunarnokg <sunarnokg1@gmail.com>
2025-01-03 22:40:35 +0700

    mengganti alamat url

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("formLink").addEventListener("click", function (event) {
-  event.preventDefault(); // Mencegah link melakukan aksi default
-  const form = document.getElementById("contactForm");
-  form.style.display = form.style.display === "none" ? "block" : "none"; // Toggle tampilan form
-});
