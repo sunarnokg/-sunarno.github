2d45e77613b24ea2d2b0ddc4ff7e9d33125d521f
sunarnokg <sunarnokg1@gmail.com>
2025-01-15 12:00:54 +0700

    menambahkan file node.js

---
+++ node.js
@@ -0,0 +1,13 @@
+{
+    "name"= "ls",
+    "version"= "1.0.0",
+    "main"= "app.js",
+    "scripts"= {
+      "start": "node app",
+      "test": "echo \"Error: no test specified\" && exit 1"
+    }
+    "author"= "sunarno",
+    "license"= "ISC",
+    "description"= ""
+  }
+  
