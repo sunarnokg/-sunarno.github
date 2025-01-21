094880efcd2994af0ba1b1e4e93ec669d5dbc9db
sunarnokg <sunarnokg1@gmail.com>
2025-01-21 12:03:34 +0700

    create server & token

--- node.js
+++ node.js
@@ -10,4 +10,32 @@
     "license"= "ISC",
     "description"= ""
   }
-  
+  // server.mjs
+import { createServer } from 'node:http';
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!\n');
+});
+
+// starts a simple http server locally on port 3000
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3000');
+});
+
+// run with `node server.mjs`
+{
+   "access_token"= "accesstoken",
+   "expires_in"= "28800",
+   "refresh_token"= "r1.refreshme",
+   "refresh_token_expires_in"= "15811200",
+   "scope"= "",
+   "token_type"="bearer"
+ }
+ POST /login/oauth/access_tokenHTTP/1.1
+Host: github.com
+
+grant_type=refresh_token
+refresh_token=r1.refreshme
+"&client_id" = xxxxxxxxxx
+"&client_secret"=xxxxxxxxxx
